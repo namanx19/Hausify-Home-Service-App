cdc5cb842c9c7756a34c13d3719c1b8281466207
namanx19 <naman.mw4@gmail.com>
2023-10-10 13:34:54 +0530

    init

--- lib/main.dart
+++ lib/main.dart
@@ -6,8 +6,12 @@ import 'package:urbanserv/Screens/home_screen.dart';
 //import 'utils/constants.dart';
 import 'package:urbanserv/Screens/start.dart';
 import 'package:urbanserv/utils/constants.dart';
+import 'package:firebase_core/firebase_core.dart';
 
-void main() {
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(const urbanserv());
 }
 
